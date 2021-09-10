091a774ceb7ab4c1a5c11121332e19be3806b5ad
Pierce <ti436522@gmail.com>
2021-09-10 12:35:52 +0100

    Trim and Seperate Data

--- Solutions/student.py
+++ Solutions/student.py
@@ -5,8 +5,36 @@ import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
 
+#region Read and Trim Data
+
 # Read Data
 # Since our data is seperated by semicolons we need to do sep=";"
 data = pd.read_csv("E:/College/Semester 7/AI/Data/student/student-mat.csv", sep=";")
 
+#print out first 5 students
 print(data.head())
+
+# This trims down our data so we select on the attributes we want to look at 
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+
+#endregion
+
+#region Seperate Data
+
+# We need to choose what attribute we want to predict
+# This will be known as the label
+# The other attributes that will determine our label are known as features
+# We will the create 2 arrays one has the label inside and the other has the features
+
+predict = "G3"
+
+X = np.array(data.drop([predict], 1)) # Features
+y = np.array(data[predict]) # Labels
+
+# We then split our data into testing and training 
+# in this case 90% data will be used to train and the other 10% will test
+# We do this so the model is not tested on data it has already seen
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
+
+#endregion
