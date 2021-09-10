49707f855e3902105dbae7224d181b179c9eba51
Pierce <ti436522@gmail.com>
2021-09-10 12:52:09 +0100

    Completed Student Tutorial

--- Solutions/student.py
+++ Solutions/student.py
@@ -25,16 +25,43 @@ data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
 # This will be known as the label
 # The other attributes that will determine our label are known as features
 # We will the create 2 arrays one has the label inside and the other has the features
-
 predict = "G3"
-
 X = np.array(data.drop([predict], 1)) # Features
 y = np.array(data[predict]) # Labels
+
+
+
 
 # We then split our data into testing and training 
 # in this case 90% data will be used to train and the other 10% will test
 # We do this so the model is not tested on data it has already seen
+# We want to test and train on both labels and features so we will have 4 arrays all together
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
+
+#endregion
+
+#region Implement Linear Regression
+
+# Define Model
+linear = linear_model.LinearRegression()
+
+# First Train then Score our models using the arrays we created
+linear.fit(x_train, y_train)
+acc = linear.score(x_test, y_test) # acc stands for accuracy 
+
+#print accuracy
+print('Accuracy: ',acc)
+
+#endregion
+
+#region Predicting Specific Students
+
+
+# Printing out all test data, next to the final grade and the predicted grade
+predictions = linear.predict(x_test) # Gets a list of all predictions
+
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
 
 #endregion
